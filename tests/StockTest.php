c617935bfbf066a56df366b6d7aac1b38a840ba2
Michał Stancelewski <michal@stancelewski.pl>
2022-01-29 19:25:57 +0100

    configuration of test database reset

--- tests/StockTest.php
+++ tests/StockTest.php
@@ -6,6 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class StockTest extends KernelTestCase {
 
+    private $entityManager;
+
+    protected function setUp() : void{
+        $kernel = self::bootKernel();
+        DatabasePrimer::prime($kernel);
+        $this->entityManager = $kernel->getContainer()->get('doctrine')->getManager();
+    }
+
     public function testItWorks(){
         $this->assertTrue(true);
     }
