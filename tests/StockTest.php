5f610ee82d3428505744c747e1645e09ad1087b8
Michał Stancelewski <michal@stancelewski.pl>
2022-01-29 19:14:02 +0100

    added php extension to reset test database

--- tests/StockTest.php
+++ tests/StockTest.php
@@ -2,9 +2,12 @@
 
 namespace App\Tests;
 
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-class StockTest{
+class StockTest extends KernelTestCase {
 
-
+    public function testItWorks(){
+        $this->assertTrue(true);
+    }
 
 }
